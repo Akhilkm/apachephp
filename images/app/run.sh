5911699a695c35acbcd1b3093c35ab5405d18e82
Akhil KM <akhilu011@gmail.com>
2020-07-05 16:36:48 +0530

    updated

--- images/app/run.sh
+++ images/app/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#magento setup:install --admin-firstname=$MAGENTO_ADMIN_FIRSTNAME \
-#       --admin-lastname=$MAGENTO_ADMIN_LASTNAME --admin-email=$MAGENTO_ADMIN_EMAIL \
-#       --admin-user=$MAGENTO_ADMIN_USER --admin-password=$MAGENTO_ADMIN_PASSWORD \
-#       --db-name=$MAGENTO_DB_NAME --db-host=$MAGENTO_DB_HOST --db-user=$MAGENTO_DB_USER \
-#       --db-password=$MAGENTO_DB_PASSWORD
+magento setup:install --admin-firstname=$MAGENTO_ADMIN_FIRSTNAME \
+      --admin-lastname=$MAGENTO_ADMIN_LASTNAME --admin-email=$MAGENTO_ADMIN_EMAIL \
+      --admin-user=$MAGENTO_ADMIN_USER --admin-password=$MAGENTO_ADMIN_PASSWORD \
+      --db-name=$MAGENTO_DB_NAME --db-host=$MAGENTO_DB_HOST --db-user=$MAGENTO_DB_USER \
+      --db-password=$MAGENTO_DB_PASSWORD
 
 apachectl -D FOREGROUND
