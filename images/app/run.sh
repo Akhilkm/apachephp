e95e551875e0920493e1d30a321e1daf0a0767a4
Akhil KM <akhilu011@gmail.com>
2020-07-06 20:31:38 +0530

    updated

--- images/app/run.sh
+++ images/app/run.sh
@@ -2,7 +2,9 @@
 
 sleep 10
 
-magento setup:install --admin-firstname=$MAGENTO_ADMIN_FIRSTNAME \
+magento setup:install --base-url=$MAGENTO_BASE_URL --backend-frontname=$MAGENTO_BACKEND_FRONTNAME \
+      --language=$MAGENTO_LANGUAGE --currency=$MAGENTO_CURRENCY --timezone=$MAGENTO_TIMEZONE 
+      --use-rewrites=$MAGENTO_USE_REWRITE --admin-firstname=$MAGENTO_ADMIN_FIRSTNAME \
       --admin-lastname=$MAGENTO_ADMIN_LASTNAME --admin-email=$MAGENTO_ADMIN_EMAIL \
       --admin-user=$MAGENTO_ADMIN_USER --admin-password=$MAGENTO_ADMIN_PASSWORD \
       --db-name=$MAGENTO_DB_NAME --db-host=$MAGENTO_DB_HOST --db-user=$MAGENTO_DB_USER \
