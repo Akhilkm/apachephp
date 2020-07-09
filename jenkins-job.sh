4f44d454535b8c798f005196cad896aea152b577
Akhil K M <Akhilkm@users.noreply.github.com>
2020-07-09 19:57:45 +0530

    Update jenkins-job.sh

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -8,6 +8,6 @@ scp -r ./* $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP:~/jenkins/workspace/
 
 ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "cp <<path to zip>> ~/jenkins/workspace/images/app/magento/"
 
-ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "~/jenkins/workspace/build.sh -o url-replace"
-ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "~/jenkins/workspace/build.sh -o build"
-ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "~/jenkins/workspace/build.sh -o deploy"
+ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "cd ~/jenkins/workspace; ./build.sh -o url-replace"
+ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "cd ~/jenkins/workspace; ./build.sh -o build"
+ssh $DOCKER_MACHINE_USER@$DOCKER_MACHINE_IP "cd ~/jenkins/workspace; ./build.sh -o deploy"
